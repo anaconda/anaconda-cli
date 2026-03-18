0e50567f84da39acef7e74644a9f53e70aa3cbd3
mattkram <mkramer@anaconda.com>
2026-03-18 17:16:57 -0500

    fix: Use delayed expansion

--- conda.recipe/run_test.bat
+++ conda.recipe/run_test.bat
@@ -1,13 +1,14 @@
 @echo on
+setlocal enabledelayedexpansion
 
 ana
 if errorlevel 1 exit /b 1
 
 for /f "delims=" %%i in ('ana') do set "actual=%%i"
-echo %actual% | findstr /C:"Hello, world! (v" >nul
+echo !actual! | findstr /C:"Hello, world! (v" >nul
 if errorlevel 1 (
   echo FAIL: Output mismatch
   echo   Expected: Hello, world! ^(v*^)
-  echo   Actual: %actual%
+  echo   Actual: !actual!
   exit /b 1
 )
